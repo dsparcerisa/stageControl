4b4362975d11438da68b52d7a121b40e1b57be5f
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-01-31 18:50:24 +0100

    distintos puertos posibles

--- commandLine_planIrradiator/shutterTests.m
+++ commandLine_planIrradiator/shutterTests.m
@@ -1,8 +1,11 @@
 % Shutter tests
 
 % 1. Connect
-% arduinoport='/dev/tty.usbserial-14340'; % for shutter
-arduinoport='/dev/tty.usbserial-14320'; % for shutter
+%arduinoport='/dev/tty.usbserial-14340'; % for shutter
+arduinoport='/dev/cu.usbserial-14340'; % for shutter
+%arduinoport='/dev/tty.usbserial-14320'; % for shutter
+%arduinoport='COM12' % Para windows?
+%%rduinoport='??' % Para Unix: hacer ls /dev y buscar cual es, o con instrfind en matlab
 
 if exist('s1') 
 else
@@ -15,6 +18,7 @@ if strcmp(s1.Status,'closed')
     s1 = serial(arduinoport,'BaudRate',9600);
     
     fopen(s1); disp('Shutter connected');
+    
         
 end
 
