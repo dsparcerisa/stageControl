9bc920fba0d32cf3ee0decd9e0a5cfae3748fd35
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-02-03 15:25:22 +0100

    new test plans

--- commandLine_planIrradiator/samplePlanIrradiation.m
+++ commandLine_planIrradiator/samplePlanIrradiation.m
@@ -1,38 +1,45 @@
-% Add path to consider 
+% Add path to consider s
 stageControlPath = '../stageControlGUI';
 addpath(stageControlPath);
 
 % Start instruments
 startInstruments
 
+planPath
 % Read plan
-plan = readPlan('plan.txt');
+plan = readPlan('plan_COM_Flash.txt');
 
 %% Data necessary for irradiation
-PPfactor = 5e-4;
-
-Ifactor = 0.81;
-
-I_FC1_nA = 2; 
-I_muestra_nA = I_FC1_nA * Ifactor * PPfactor;
-
-plan.t_s = plan.Q ./ I_muestra_nA / 1000;
+% PPfactor = 5e-4;
+% 
+% Ifactor = 0.81;
+% 
+% I_FC1_nA = 2; 
+% I_muestra_nA = I_FC1_nA * Ifactor * PPfactor;
+% 
+% plan.t_s = plan.Q ./ I_muestra_nA / 1000;
 
 %% Center stage
 autoCenter(s2);   
 [~, ~, ~, Zpos] = monitorStatus(s2);
 
 %% Align in Z position
-% align manually and then set here
+% align manually and then set here (EN EL ASPA)
 Zpos = 0;
+stageControlStart;
+
+%% Calculo del vector global
+% Aspa al Pocillo 0 0
+%deltaX = +95.5 mm
+%deltaY = +63 mm
+X2poc00 = [9.55 6.3 0];
+
+% Pocillo 0 0 al centro de la placa:
+poc00toplateCtr = [-0.899*5.5 -0.899*3.5 0];
+
+globalVector = X2poc00 + poc00toplateCtr
 
 %% Irradiate plan
 tic
-for i=1:numel(plan.X)
-    absPos = [plan.X(i) plan.Y(i) Zpos];
-    finished = stageControl_moveToAbsPos(s2, absPos);
-    Configure_shutter(s1,'t',plan.t_s(i));
-    Shutter(s1,'n',1);
-    fprintf('Irradiated spot %i\n', i);
-end
+irradiatePlan(s2, s1, plan, globalVector)
 toc
